a8f1c14676564cd5ebc1666ca7448f23b406a59e
Roshni Sensarma <roshnisensarma@Roshnis-Laptop.local>
2026-02-20 17:40:51 -0800

    Add daily Dagster schedule

--- dq_autopilot/definitions.py
+++ dq_autopilot/definitions.py
@@ -1,4 +1,21 @@
-from dagster import Definitions
+# from dagster import Definitions
+# from dq_autopilot.resources.postgres import postgres
+# from dq_autopilot.assets.dq_assets import (
+#     discovered_tables,
+#     table_profiles,
+#     write_dq_results,
+#     dq_failures,
+#     dq_alerts,
+# )
+
+# defs = Definitions(
+#     assets=[discovered_tables, table_profiles, write_dq_results, dq_failures, dq_alerts],
+#     resources={"postgres": postgres},
+# )
+
+#Automated script for 9am runs
+
+from dagster import Definitions, ScheduleDefinition, define_asset_job
 from dq_autopilot.resources.postgres import postgres
 from dq_autopilot.assets.dq_assets import (
     discovered_tables,
@@ -8,31 +25,16 @@ from dq_autopilot.assets.dq_assets import (
     dq_alerts,
 )
 
+dq_job = define_asset_job(name="dq_autopilot_job")
+
+daily_9am = ScheduleDefinition(
+    name="daily_dq_autopilot_9am",
+    cron_schedule="0 9 * * *",
+    job=dq_job,
+)
+
 defs = Definitions(
     assets=[discovered_tables, table_profiles, write_dq_results, dq_failures, dq_alerts],
     resources={"postgres": postgres},
+    schedules=[daily_9am],
 )
-
-#Automated script for 9am runs
-# from dagster import Definitions, ScheduleDefinition, define_asset_job
-# from dq_autopilot.resources.postgres import postgres
-# from dq_autopilot.assets.dq_assets import (
-#     discovered_tables,
-#     table_profiles,
-#     write_dq_results,
-#     dq_alerts,
-# )
-
-# dq_job = define_asset_job(name="dq_autopilot_job")
-
-# daily_9am = ScheduleDefinition(
-#     name="daily_dq_autopilot_9am",
-#     cron_schedule="0 9 * * *",
-#     job=dq_job,
-# )
-
-# defs = Definitions(
-#     assets=[discovered_tables, table_profiles, write_dq_results, dq_alerts],
-#     resources={"postgres": postgres},
-#     schedules=[daily_9am],
-# )
